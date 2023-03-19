53dc1ca2f794235f22266736d644e284df6b7737
fastpostya <fastpost@rambler.ru>
2023-03-20 00:32:37 +0300

    class Connector finished

---
+++ tests/test_connector.py
@@ -0,0 +1,149 @@
+from json import JSONDecodeError
+import os
+import unittest
+from utils.connector import Connector
+
+
+class Test_Connector(unittest.TestCase):
+    """Class for testing class Connector."""
+
+    def setUp(self):
+        """setting initial values"""
+        self.path_open = os.sep.join(["tests", "test_files", "test_open.json"])
+        self.path_no_exist = os.sep.join(["tests",
+            "test_files", "nofile.json"])
+        self.path_no_exist_and_del = os.sep.join(["tests",
+            "test_files", "nofile1.json"])
+        self.path_old_file = os.sep.join(["tests", "test_files",
+            "result_hh.json"])
+        self.path_new = os.sep.join(["tests", "test_files",
+            "new.json"])
+        # create new file for testing
+        file = open(self.path_new, "w", encoding="UTF-8")
+        file.write('[{"key1": "value1"}, {"key2": "value2"}]')
+        file.close()
+
+        self.path_empty_insert = os.sep.join(["tests", "test_files",
+            "test__empty_insert.json"])
+        file = open(self.path_empty_insert, "w", encoding="UTF-8")
+        file.write('')
+        file.close()
+
+        self.path_save = os.sep.join(["tests", "test_files", "test_save.json"])
+        self.valid_json = '[{"key1": "value1"}, {"key2": "value2"}]'
+        self.connector = Connector(self.path_new)
+
+        self.path_select = os.sep.join(["tests", "test_files",
+            "test_select.json"])
+        # create new file for testing
+        file = open(self.path_select, "w", encoding="UTF-8")
+        file.write('')
+        file.close()
+
+        self.path_del = os.sep.join(["tests", "test_files", "test_delete.json"])
+        # create new file for testing
+        file = open(self.path_del, "w", encoding="UTF-8")
+        file.write('')
+        file.close()
+
+    def test__init__(self) -> None:
+        """Testing initialising instanse of class Connector.
+        testing if path exist and is actual. Else raises
+        JSONDecodeError or TimeoutError.
+        """
+        self.assertEqual(self.connector.data_file, os.sep.join(["tests", "test_files",
+            "new.json"]))
+
+    def test_read_file(self) -> str:
+        """Test if file exist - read and return text,
+        if not - create it and return '' """
+        self.assertEqual(
+            self.connector.read_file(),
+            '[{"key1": "value1"}, {"key2": "value2"}]')
+        self.assertEqual(
+            self.connector.text,
+            '[{"key1": "value1"}, {"key2": "value2"}]')
+        connector_del = Connector(self.path_no_exist_and_del)
+        self.assertEqual(connector_del.read_file(), '')
+
+        # del file self.path_no_exist_and_del after testing
+        if os.path.isfile(self.path_no_exist_and_del):
+            os.remove(self.path_no_exist_and_del)
+
+    def test_is_file_not_old(self) -> bool:
+        """test if file was create more than 24-h til now"""
+        self.assertTrue(self.connector.is_file_not_old(self.path_new))
+        with self.assertRaises(TimeoutError):
+            self.connector.is_file_not_old(self.path_old_file)
+
+    def test_is_valid_json(self) -> bool:
+        """testing is json correct"""
+        self.assertTrue(self.connector.is_valid_json('[{"key1": "value1"},\
+ {"key2": "value2"}]'))
+        self.assertTrue(self.connector.is_valid_json('{"key1": "value1"}'))
+        self.assertTrue(self.connector.is_valid_json('{}'))
+        with self.assertRaises(JSONDecodeError):
+            self.connector.is_valid_json('')
+        with self.assertRaises(JSONDecodeError):
+            self.connector.is_valid_json('[{value1}, {"key2": "value2"}]')
+        with self.assertRaises(JSONDecodeError):
+            self.connector.is_valid_json('[{"key1": "value1"}, {"value2"}]')
+
+    def test_save_date(self) -> None:
+        """testing saving file"""
+        connector_save = Connector(self.path_save)
+        connector_save.save_date(self.valid_json)
+        self.assertTrue(os.path.exists(self.path_save))
+
+    def test_insert(self) -> None:
+        """testing inserting in empty file and not empty"""
+        self.connector_insert = Connector(self.path_empty_insert)
+        self.assertEqual(
+                        self.connector_insert.insert(
+                            '{"key": "value"}'),
+                            '{"key": "value"}')
+        self.assertEqual(self.connector_insert.insert('{"key1": "value1"}'),
+            '[{"key": "value"}, {"key1": "value1"}]')
+        self.assertEqual(self.connector_insert.insert('{"key2": "value2"}'),
+            '[{"key": "value"}, {"key1": "value1"}, {"key2": "value2"}]')
+        with self.assertRaises(KeyError):
+            self.connector_insert.insert("")
+        # create empty file
+        file = open(self.path_empty_insert, "w", encoding="UTF-8")
+        file.write('')
+        file.close()
+        self.connector_insert = Connector(self.path_empty_insert)
+        self.assertEqual(self.connector_insert.insert('[{"key": "value"},\
+ {"key1": "value1"}]'), '[{"key": "value"}, {"key1": "value1"}]')
+        self.assertEqual(self.connector_insert.insert('{"key2": "value2"}'),
+            '[{"key": "value"}, {"key1": "value1"}, {"key2": "value2"}]')
+
+    def test_select(self) -> None:
+        """testing selection from file"""
+        self.connector_select = Connector(self.path_select)
+        with self.assertRaises(KeyError):
+            self.connector_select.select({"key": "value"})
+        self.connector_select.insert('[{"key4": "value4", "key6": "value6"}, \
+    {"key5": "value5"}, {"key5": "value6", "text1": "1235"}, {"key6": "value6"\
+    , "text2": "4587"}, {"key6": "value6", "tex32": "2587"}]')
+
+    def test_delete(self) -> None:
+        """testin deleting file by query"""
+        self.connector_del = Connector(self.path_del)
+        self.connector_del.insert('[{"key4": "value4", "key6": "value6"},\
+ {"key5": "value5"}, {"key5": "value6", "text1": "1235"},\
+ {"key6": "value6", "text2": "4587"}, {"key6": "value6", "tex32": "2587"}]')
+        self.connector_del.delete({"key5": "value5"})
+        self.assertEqual(self.connector_del.text,
+        '[{"key4": "value4", "key6": "value6"}, {"key5": "value6",\
+ "text1": "1235"}, {"key6": "value6", "text2": "4587"}, {"key6":\
+ "value6", "tex32": "2587"}]')
+        self.connector_del.delete({"key4": "value4"})
+        self.assertEqual(self.connector_del.text,
+        '[{"key5": "value6",\
+ "text1": "1235"}, {"key6": "value6", "text2": "4587"}, {"key6":\
+ "value6", "tex32": "2587"}]')
+        with self.assertRaises(KeyError):
+            self.connector_del.delete({})
+        with self.assertRaises(KeyError):
+            self.connector_del.delete("")
--- utils/connector.py
+++ utils/connector.py
@@ -2,30 +2,40 @@ import datetime
 import os
 import json
 from json import JSONDecodeError
-import pprint
-# from vacancy import Vacancy
 
 
 class Connector:
     """
     The Class Connector is  connector to the file.
     The file must be in json format.
-    In method is_valid_json is cheking for:
-     - file is in JSON-format
-     -
-    Класс коннектор к файлу, обязательно файл должен быть в json формате
-    не забывать проверять целостность данных, что файл с данными не подвергся
-    внешней деградации
+
+    Methods:
+    - is_valid_json: bool - cheking if file is in JSON-format.
+    - is_file_exist: bool -cheking if file exists
+    - read_file: str - getting text from file data_file
+    - is_file_not_old: bool - checking if file not older than number days
+    - save_date: str - saving data in file data_file
+    - insert: str - insert date in file with saving it's structure.
+    - select: list | KeyError - selecting dates from file.
+        The key is the field for filtering. The value is the desired value.
+    - delete: list | KeyError - deleting date, using query.
+
+    Atributes:
     :param data_file:str - the path to the json-file. It has getter and setter.
-    :param text: str - the text from the file 
+    :param text: str - the text from the file
+    :param save_date: list - list for saving in file
+    :param select_query: dict - dict for selection data
+    :param 
     """
     __data_file = None
 
     def __init__(self, path):
         self.data_file = path
         self.text = ""
+        self.list_select = ""
+        self.select_query = {}
+        self.delete_query = {}
         self.__connect()
-        # self.elements = []
 
     @property
     def data_file(self):
@@ -49,7 +59,6 @@ class Connector:
             with open(path, "r", encoding="utf-8") as file:
                 text = file.read()
                 self.text = text
-                # print("path=", path, " self.text=",self.text)
             return self.text
         else:
             # file does not exist, let's create it
@@ -60,37 +69,34 @@ class Connector:
 
     @staticmethod
     def is_file_not_old(path: str, days: int = 1) -> bool | TimeoutError:
-        """ Cheking file creating date and time. If the file was 
-        creating more then <days> ago raises TimeoutError, in other case returns True"""
+        """ Cheking file creating date and time. If the file was
+        creating more then <days> ago raises TimeoutError,
+        in other case returns True
+        """
         file_time = os.path.getmtime(path)
         file_time = datetime.datetime.fromtimestamp(file_time)
-        time_now = datetime.datetime.now()         
+        time_now = datetime.datetime.now()
         time_delta = time_now - file_time
         if time_delta > datetime.timedelta(days=days):
-            raise TimeoutError(f"Файл с данными устарел.\
+            raise TimeoutError("Файл с данными устарел.\
  Пожалуйста, обновите его!")
-            # return False
         return True
 
     @staticmethod
     def is_valid_json(text: str) -> bool | JSONDecodeError:
         """
-        Raises JSONDecodeError if text is not JSON or 
+        Raises JSONDecodeError if text is not JSON or
         return True if json is correct.
-        :param text: str - text for checking      
+        :param text: str - text for checking
         """
-        # try:
-        text_json = json.loads(text)            
-        # except JSONDecodeError:
-            # Файл self.data_file содержит некорректные данные или пустой.
-            # return False
+        text_json = json.loads(text)
         return True
 
-    def save_date(self, text: str) -> None:
+    def save_date(self, data: dict | list) -> None:
         """Saving data in file data_file"""
         with open(self.data_file, "w", encoding="utf-8") as file:
-            json.dump(text, file)
-            return text
+            json.dump(data, file)
+            return data
 
     def __connect(self) -> str | Exception:
         """
@@ -104,18 +110,15 @@ class Connector:
 
     def insert(self, data: str) -> str:
         """
-        Insert date in file with saving it's structure
+        Insert date in file with saving it's structure.
+        :param data: str - date in JSON format.
+        In case empty or wrong date raises KeyError.
+        Returns text - date with insertion.
         """
         # open file and read date from file
         # file was checked while initialisation
-        # self.read_file()
-        # print(self.text)
         list_save = []
-        if self.text == "":
-            # empty file
-            self.save_date(data)
-            return data
-        else:
+        if self.text != "":
             # not empty file
             date_from_file = json.loads(self.text)
             if isinstance(date_from_file, dict):
@@ -138,78 +141,107 @@ class Connector:
                 raise KeyError("Некорректные данные")
         else:
             raise KeyError("Некорректные данные")
+        if len(list_save) == 1:
+            self.save_date(list_save[0])
+            self.text = json.dumps(list_save[0])
+        else:
+            self.save_date(list_save)
+            self.text = json.dumps(list_save)
+        return self.text
 
-        self.save_date(list_save)
-        return list_save
-
-    def select(self, query):
+    def select(self, query: dict) -> list | KeyError:
         """
-        Выбор данных из файла с применением фильтрации
-        query содержит словарь, в котором ключ - это поле для
-        фильтрации, а значение - это искомое значение, например:
-        {'price': 1000}, должно отфильтровать данные по полю price
-        и вернуть все строки, в которых цена 1000
+        Selecting dates from file.
+        :param query:dict - the dictinary for selection date.
+        Selected dates are saving in list_select
+        The key is the field for filtering. The value is the desired value.
+        Example:
+        {'price': 1000} return only dictionary that contain the field 'price'
+        and price is equal 1000.
+        In case query contain more or less than 1 pair key/value
+        or file is empty raises KeyError.
         """
+        self.select_query = query
         self.read_file()
+        self.list_select = []
         if self.text != "":
             date = json.loads(self.text)
-            filtred_list = []
-            for element in date:
-                if isinstance(query, dict):
-                    if dict.keys in element and dict.values == element[dict.keys]:
-                        filtred_list.append(element)
-            return filtred_list
+            self.list_select = []
+            if isinstance(query, dict) and len(query) == 1:
+                first_key = list(query.keys())[0]
+                first_value = query[first_key]
+                for element in date:
+                    if first_key in element and first_value ==\
+                            element[first_key]:
+                        self.list_select.append(element)
+
+                return self.list_select
+            else:
+                raise (KeyError("Неправильный формат \
+словаря для отбора данных"))
         else:
             raise (KeyError("Неправильный формат JSON-файла"))
 
-    def delete(self, query):
+    def delete(self, query: dict) -> list | KeyError:
         """
-        Удаление записей из файла, которые соответствуют запросу,
-        как в методе select. Если в query передан пустой словарь, то
-        
-        функция удаления не сработает
+        Deleting date, using query.
+        :param query:dict - the dictinary for deleting date.
+        The key is the field for filtering. The value is the desired value.
+        In case query contain more or less than 1 pair key/value
+        or file is empty raises KeyError.
         """
-        pass
+        self.delete_query = query
+        self.read_file()
+        if self.text != "":
+            date = json.loads(self.text)
+            new_list = []
+            if isinstance(query, dict) and len(query) == 1:
+                first_key = list(query.keys())[0]
+                first_value = query[first_key]
+                for element in date:
+                    if not (first_key in element and first_value ==\
+                            element[first_key]):
+                        new_list.append(element)
+                self.save_date(new_list)
+                self.text = json.dumps(new_list)
+                return new_list
+            else:
+                raise (KeyError("Неправильный формат словаря для отбора данных"))
+        else:
+            raise (KeyError("Неправильный формат JSON-файла"))
+
 
 def del_file(path):
     file = open(path, "w", encoding="UTF-8")
     file.write('')
     file.close()
 
-if __name__ == '__main__':
 
-    # path_save = os.sep.join(["tests", "test_files", "new.json"])
-    # text_json = json.loads(path_save) 
-    # print(text_json)
-    # print(type(text_json))
-
+def try_insert():
     path_insert = os.sep.join(["tests", "test_files", "test_insert.json"])
-    # del_file(path_insert)
-
-    # with open(path_insert, "r", encoding="utf-8") as file:
-    #     text = file.read()
-    # print(text)
-    # with open(path_insert, "r", encoding="utf-8") as file:
-    #     text = file.read()
-    # print(text)
-    # with open(path_insert, "r", encoding="utf-8") as file:
-    #     text = file.read()
-    # print(text)
-
-    # ---SELECT---
-    # path_select = os.sep.join(["tests", "test_files", "test_select.json"])
-    # connector_select = Connector(path_select)
-    # connector_select.select({"key6": "value6"})
-    # print(connector_select.text)
-# -------------------------
-# ----INSERT----
+    # clear file
+    del_file(path_insert)
+    # ----INSERT----
     connector_insert = Connector(path_insert)
     connector_insert.insert('{"key4": "value4"}')
     print(connector_insert.text)
-    # connector_insert.insert('{"key5": "value5"}')
-    # print(connector_insert.text)
-    # connector_insert.insert('{"key6": "value6"}')
-    # print(connector_insert.text)
+    connector_insert.insert('{"key5": "value5"}')
+    print(connector_insert.text)
+    try:
+        connector_insert.insert('[{"key6": "value6"}, {"text1": "1235"}]')
+        print(connector_insert.text)
+    except:
+        print('ERROR:{"key6": "value6", "text1": "1235"}')
+    try:
+        connector_insert.insert('[{"key6": "value6", "text2": "4587"}]')
+        print(connector_insert.text)
+    except:
+        print('ERROR: {"key6": "value6"}, {"text2": "4587"}')
+    try:
+        connector_insert.insert('[{"key6": "value6", "tex32": "2587"}]')
+        print(connector_insert.text)
+    except:
+        print('ERROR: {"key6": "value6"}, "tex32": "2587"}')
     # # # try:
     # connector_insert.insert('')
     # print(connector_insert.text)
@@ -221,14 +253,32 @@ if __name__ == '__main__':
     # pprint.pprint(connector_insert.text)
 
 
-    # path = os.sep.join(["tests", "test_files", "test_insert.json"])
-    # connector = Connector(path)
-    # print(connector.read_file())
-    # connector.insert('[{"key": "value"}, {"key": "value1"}]')
-    # path1 = connector.data_file
-    # with open(path1, "r", encoding="utf-8") as file:
-    #     text = file.read()
-    # print("text=", text)
+def try_select():
+    # ---SELECT---
+    path_select = os.sep.join(["tests", "test_files", "test_select.json"])
+    connector_select = Connector(path_select)
+    connector_select.select({"key6": "value6"})
+    print(connector_select.list_select)
+
+
+def try_delete():
+    path_delete = os.sep.join(["tests", "test_files", "test_delete.json"])
+    del_file(path_delete)
+    connector_del = Connector(path_delete)
+    connector_del.insert('[{"key4": "value4"}, {"key5": "value5"}, {"key6": "value6"}, {"text1": "1235"}, {"key6": "value6", "text2": "4587"}]')
+    connector_del.delete({"text1": "1235"})
+    connector_del.delete({"key5": "value5"})
+    connector_del.delete({"key4": "value4"})
+    connector_del.delete({"key6": "value6"})
+    connector_del.insert('[{"key4": "value4"}, {"key5": "value5"}, {"key6": "value6"}, {"text1": "1235"}, {"key6": "value6", "text2": "4587"}]')
+
+    print(connector_del.text)
+
+if __name__ == '__main__':
+    try_insert()
+    # try_select()
+    # try_delete()
+
 
     # try:
         # df = Connector(os.sep.join(["vacations.json"]))
@@ -242,30 +292,3 @@ if __name__ == '__main__':
     #     print("Неверный формат JSON")
     # except TimeoutError:
     #     print("Файл устарел")
-
-    # connector_save = Connector(os.sep.join(["tests", "test_files", "test_save.json"]))
-    # connector_save.save_date('[{"key1": "value1"}, {"key2": "value2"}]')
-
-        # path_old_file = os.sep.join(["tests", "test_files", 
-        #     "result_hh.json"])
-        # path_new = os.sep.join(["tests", "test_files", 
-        #     "new.json"])
-
-    #     print(df.is_file_not_old(path_old_file))
-    #     print(df.is_file_not_old(path_new))
-    # except TimeoutError as error:
-    #     print(error)
-    # except ValueError:
-    #     print("Структура json-файла неверная. Невозможно прочитать данные.")
-    # df.__connect()
-    # df = Connector('df.json')
-
-    # data_for_file = {'id': 1, 'title': 'tet'}
-
-    # df.insert(data_for_file)
-    # data_from_file = df.select(dict())
-    # assert data_from_file == [data_for_file]
-
-    # df.delete({'id':1})
-    # data_from_file = df.select(dict())
-    # assert data_from_file == []
